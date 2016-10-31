2f785140e21afb8ac8a9abd361185d33b6a42521
airobert <ai.robert.wangshuai@gmail.com>
2016-10-31 15:38:43 +0100

    first

---
+++ homework.R
@@ -0,0 +1,16 @@
+# Some useful links
+# http://mathesaurus.sourceforge.net/r-numpy.html
+A = matrix(c(3,-4,1,2,2,0),2,3)
+
+D = matrix(rep(0,15), 3,5)
+
+diag(1,4)
+
+addOne <- function (x) {
+	return (x + 1)
+}
+
+plot(x, y, type = 'l', col = 'blue')
+
+savePlot('sin.png', type='png')
+
