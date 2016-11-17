7174d7ef161db19967662de91a4bd017c41122a9
Robert White <ai.robert.wangshuai@gmail.com>
2016-11-17 10:54:00 +0100

    png

---
+++ homework3.R
@@ -0,0 +1,132 @@
+
+library('fields')
+library('deSolve')
+
+
+dvdr = function(v, r, params) { # the vector contains the parameters a, b, c, d
+	a = params[1]
+	b = params[2]
+	c = params[3]
+	I = params[4]
+	# return a vector contain dv and dr
+	d_v = c * (v - 1/3 * (v^3) + r + I)
+	d_r = -1 / c * (v - a + b * r)
+	return (c(d_v, d_r))
+}
+
+
+
+
+
+v_nullcline = function (vv, params){
+	a = params[1]
+	b = params[2]
+	c = params[3]
+	I = params[4]
+	rr = rep (0, length(vv))
+	for (i in 1:length(vv)){
+		rr[i] = 1/3 * (vv[i])^3 - vv[i] - I 
+	}
+	return (rr)
+}
+
+
+r_nullcline = function (vv, params){
+	a = params[1]
+	b = params[2]
+	c = params[3]
+	I = params[4]
+	rr = rep (0, length(vv))
+	for (i in 1:length(vv)){
+		rr[i] = (a - vv[i]) / b 
+	}
+	return (rr)
+}
+
+
+
+
+
+
+
+x = seq(0,30, 0.5)
+y = rep(0, length(x))
+dx = sin(x)
+
+plot (x,y, type='n')
+# plot (x,dx, type='n')
+arrow.plot(x, 0, dx, 0, arrow.ex = 0.05, length = 0.05, col = 'blue', lwd = 1, true.angle=TRUE)
+
+
+
+xmin = -2; xmax = 2; xstep = 0.2
+ymin = -2; ymax = 2; ystep = 0.2
+
+
+x = seq(xmin, xmax, xstep)
+y = seq(ymin, ymax, ystep)
+
+x_values = rep (x, time = length(y))
+y_values = rep (y, each = length(x))
+
+
+
+x_dirs = rep (0, length(x_values))
+y_dirs = rep (0, length(y_values))
+
+# params = c(1,2,-2,-1)
+params = c(0.7 , 0.8, 3 , 0)
+
+
+for (i in 1:length(x_values)) {
+	dir = dvdr(x_values[i], y_values [i], params)
+	x_dirs [i] = dir[1]
+	y_dirs [i] = dir[2]
+}
+
+plot (x,y, type='n', xlab = 'dx', ylab = 'dy')
+arrow.plot(x_values, y_values, x_dirs, y_dirs, arrow.ex = 0.05, length = 0.05, col = 'blue', lwd = 1, true.angle=TRUE)
+
+
+
+x = seq(xmin, xmax, xstep/10)
+y_x = v_nullcline(x, params)
+y_y = r_nullcline(x, params)
+
+
+lines (x, y_x, col = 'blue',lty=1,lwd=3)
+lines (x, y_y, col = 'red',lty=1,lwd=3)
+
+
+
+
+ode_system = function (t, state, params) {
+	v = state [1]
+	r = state [2]
+	return (list(dvdr(v,r,params)))
+}
+
+
+
+
+
+
+x_iter = seq(xmin, xmax, xstep)
+y_iter = seq(ymin, ymax, ystep)
+
+for (x_init in x_iter) {
+	for (y_init in y_iter) {
+
+		times = seq(1, 5, 0.01)
+
+		points (x_init, y_init)
+
+		trajectory <- ode (y = c(y1=x_init, y2=y_init), 
+			times = times,
+			func = ode_system, 
+			parms = params)
+
+		lines(trajectory[,2], trajectory[,3], col = 43)
+		
+	}
+}
